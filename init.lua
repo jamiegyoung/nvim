34b5ecc53cbe8833ae862b618410242c7b008644
jamiegyoung <jamiegyoung22@gmail.com>
2024-06-17 11:47:37 +0100

    feat: add twig formating

--- init.lua
+++ init.lua
@@ -504,6 +504,7 @@ require("lazy").setup({
 					cmd = { "bundle", "exec", "rubocop", "--lsp" },
 					root_dir = require("lspconfig").util.root_pattern("Gemfile", ".git", "."),
 				},
+				twiggy_language_server = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
@@ -587,6 +588,7 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				djlint = { "twig" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
