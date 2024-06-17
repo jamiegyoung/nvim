a399495c23e568eca2531eadcc44a295f84ccb03
jamiegyoung <jamiegyoung22@gmail.com>
2024-06-17 11:46:49 +0100

    feat: clear all whitespace on save

--- init.lua
+++ init.lua
@@ -4,6 +4,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
