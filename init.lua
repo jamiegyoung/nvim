c49e66e0146b5c44c4898ffdea9214686b2d2fcc
jamiegyoung <jamiegyoung22@gmail.com>
2024-06-27 11:00:00 +0100

    feat: enable copilot for all filetypes

--- init.lua
+++ init.lua
@@ -838,7 +838,10 @@ require("lazy").setup({
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
 		event = "InsertEnter",
-		opts = { suggestion = { auto_trigger = true, debounce = 150 } },
+		opts = {
+			suggestion = { auto_trigger = true, debounce = 150 },
+			filetypes = { ["*"] = true },
+		},
 	},
 	{
 		"kdheepak/lazygit.nvim",
