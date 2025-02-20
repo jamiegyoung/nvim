15c3cb91a5e4d9108f25c266aa5757f5e2bfe1a3
jamiegyoung <jamie@fullfatthings.com>
2025-02-20 11:28:12 +0000

    feat(init): add Jamtest command for inserting time-based UUIDs

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -6,3 +6,18 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 -- Set the color columns to 80 and 120
 vim.cmd("set colorcolumn=80,120")
+
+vim.api.nvim_create_user_command('Jamtest', function()
+    -- Get the UUID using uuidgen -t
+    local uuid = 'jamtest-' .. vim.fn.system('uuidgen -t'):gsub('%s+', '')
+
+    -- Get the current cursor position
+    local pos = vim.api.nvim_win_get_cursor(0)
+    local row = pos[1] - 1
+    local col = pos[2]
+
+    -- Insert the UUID at the cursor position
+    vim.api.nvim_buf_set_text(0, row, col, row, col, {uuid})
+end, {
+    desc = "Insert a time-based UUID at cursor position"
+})
