09ba3eef6bcf2b87467a4b058bfb6b06e0c8e5d1
jamiegyoung <jamie@fullfatthings.com>
2025-07-29 12:22:23 +0100

    feat(plugins): replace codecompanion with avante.nvim for AI assistance

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -12,15 +12,15 @@ return {
 		end,
 		ft = { "markdown" },
 	},
-	-- {
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	cmd = "Copilot",
-	-- 	event = "InsertEnter",
-	-- 	opts = {
-	-- 		suggestion = { auto_trigger = true, debounce = 150 },
-	-- 		filetypes = { ["*"] = true },
-	-- 	},
-	-- },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		opts = {
+			suggestion = { auto_trigger = true, debounce = 150 },
+			filetypes = { ["*"] = true },
+		},
+	},
 	-- {
 	-- 	"CopilotC-Nvim/CopilotChat.nvim",
 	-- 	branch = "canary",
@@ -36,31 +36,86 @@ return {
 	-- 		{ "<leader>cc", "<cmd>CopilotChat<cr>", desc = "Open Copilot Chat" },
 	-- 	},
 	-- },
+	-- {
+	-- 	"olimorris/codecompanion.nvim",
+	-- 	config = function()
+	-- 		require("codecompanion.adapters").extend("gemini", {
+	-- 			env = {
+	-- 				api_key = function()
+	-- 					return os.getenv("GEMINI_API_KEY")
+	-- 				end,
+	-- 				model = "schema.model.default",
+	-- 			}
+	-- 		})
+	-- 		require("codecompanion").setup({
+	-- 			strategies = {
+	-- 				chat = {
+	-- 					adapter = "gemini",
+	-- 				},
+	-- 				inline = {
+	-- 					adapter = "gemini",
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 	},
+	-- },
 	{
-		"olimorris/codecompanion.nvim",
-		config = function()
-			require("codecompanion.adapters").extend("gemini", {
-				env = {
-					api_key = function()
-						return os.getenv("GEMINI_API_KEY")
-					end,
-					model = "schema.model.default",
-				}
-			})
-			require("codecompanion").setup({
-				strategies = {
-					chat = {
-						adapter = "gemini",
-					},
-					inline = {
-						adapter = "gemini",
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = "*", -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+		opts = {
+			-- add any opts here
+			-- for example
+			provider = "gemini",
+			gemini = {
+				model = "gemini-2.0-flash"
+			}
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
 					},
 				},
-			})
-		end,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				'MeanderingProgrammer/render-markdown.nvim',
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
 		},
 	},
 	{
